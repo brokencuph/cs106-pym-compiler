7f786c094b448afd2d26d7ef8ecf8a00aaaf500e
brokencuph <415947405@qq.com>
2020-12-23 00:03:48 +0800

    implement some of the stmts

--- PymParser/parser.cpp
+++ PymParser/parser.cpp
@@ -1,5 +1,6 @@
 #include <stdexcept>
 #include <memory>
+#include <cstring>
 
 #include "parser.h"
 
@@ -32,34 +33,7 @@ SharedTreeNode Parser::statement_list()
 	auto curStmtPos = &root->children[0]; // address of write position
 	while (pos != content.cend() && pos->type != TokenType::FEOF)
 	{
-		auto nextPos = ++std::list<Token>::const_iterator(pos);
-		auto stmt = [this, nextPos]() -> SharedTreeNode {
-			switch (pos->type)
-			{
-			case TokenType::DEF:
-				return def_stmt();
-			case TokenType::IF:
-				return if_stmt();
-			case TokenType::WHILE:
-				return while_stmt();
-			case TokenType::RETURN:
-				return return_stmt();
-			case TokenType::INDENT:
-				return compound_stmt();
-			case TokenType::ID:
-				if (nextPos != content.cend() && nextPos->type == TokenType::COLON)
-				{
-					return decl_stmt();
-				}
-				else
-				{
-					return expr_stmt();
-				}
-			default:
-				return expr_stmt();
-			}
-		};
-		*curStmtPos = stmt();
+		*curStmtPos = statement();
 		curStmtPos = &((*curStmtPos)->rSibling);
 		pos++;
 	}
@@ -67,5 +41,51 @@ SharedTreeNode Parser::statement_list()
 
 SharedTreeNode Parser::statement()
 {
+	auto nextPos = ++std::list<Token>::const_iterator(pos);
+	switch (pos->type)
+	{
+	case TokenType::DEF:
+		return def_stmt();
+	case TokenType::IF:
+		return if_stmt();
+	case TokenType::WHILE:
+		return while_stmt();
+	case TokenType::RETURN:
+		return return_stmt();
+	case TokenType::INDENT:
+		return compound_stmt();
+	case TokenType::ID:
+		if (nextPos != content.cend() && nextPos->type == TokenType::COLON)
+		{
+			return decl_stmt();
+		}
+		else
+		{
+			return expr_stmt();
+		}
+	default:
+		return expr_stmt();
+	}
+}
 
+SharedTreeNode Parser::def_stmt()
+{
+	if (pos->type != TokenType::DEF)
+	{
+		throw std::invalid_argument("DEF expected");
+	}
+	pos++;
+	if (pos->type != TokenType::ID)
+	{
+		throw std::invalid_argument("identifier expected");
+	}
+	char* id = new char[pos->str.size() + 1];
+	strcpy(id, pos->str.c_str());
+	pos++;
+	if (pos->type != TokenType::LPR)
+	{
+		throw std::invalid_argument("( expected");
+	}
+	pos++;
+	// not finished
 }
